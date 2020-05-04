f1afe840562001cd9cca126db83991eb58e313fd
stefanstifter <stefan.stifter@saswebdev.com>
2020-05-04 20:59:58 +0200

    clean up

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="container mx-auto px-4 sm:px-6 lg:px-8 bg-gray-300">
+      <header className="">
+        <h1 className="font-sans text-xl font-semibold">Is there something to do?</h1>
       </header>
+      <ul>
+        <li>
+          <div class="bg-white overflow-hidden shadow rounded-lg">
+            <div class="border-b border-gray-200 px-4 py-5 sm:px-6">
+              Header
+            </div>
+            <div class="px-4 py-5 sm:p-6">
+              Body
+            </div>
+          </div>
+        </li>
+        <li>2</li>
+        <li>3</li>
+        <li>4</li>
+      </ul>
     </div>
   );
 }
