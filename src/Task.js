849d9f0812b12fc164e326731e2b70d28d60c2e7
stefanstifter <stefan.stifter@saswebdev.com>
2020-05-04 21:55:19 +0200

    wip

--- src/Task.js
+++ src/Task.js
@@ -3,26 +3,26 @@ import React from 'react';
 class Task extends React.Component {
   render() {
     return (
-      <div class="bg-white overflow-hidden shadow rounded-lg">
-        <div class="border-b border-gray-200 px-4 py-5 sm:px-6">
-          <span class="inline-flex items-center px-3 py-0.5 rounded-full text-sm font-medium leading-5 bg-red-100 text-red-800">
-            <svg class="-ml-1 mr-1.5 h-2 w-2 text-red-400" fill="currentColor" viewBox="0 0 8 8">
+      <div className="bg-white overflow-hidden shadow rounded-lg">
+        <div className="border-b border-gray-200 px-4 py-5 sm:px-6">
+          <span className="inline-flex items-center px-3 py-0.5 rounded-full text-sm font-medium leading-5 bg-red-100 text-red-800">
+            <svg className="-ml-1 mr-1.5 h-2 w-2 text-red-400" fill="currentColor" viewBox="0 0 8 8">
               <circle cx="4" cy="4" r="3" />
             </svg>
-            <span class="ml-2">
+            <span className="ml-2">
               not done yet
             </span>
           </span>
-          <h2 class="mt-1 text-xl">
+          <h2 className="mt-1 text-xl">
             { this.props.title }
           </h2>
         </div>
-        <div class="px-4 py-5 sm:p-6">
+        <div className="px-4 py-5 sm:p-6">
           { this.props.body }
         </div>
-        <div class="border-t border-gray-200 px-4 py-4 sm:px-6">
-        <span class="inline-flex rounded-md shadow-sm">
-          <button type="button" class="inline-flex items-center px-4 py-2 border border-gray-300 text-sm leading-5 font-medium rounded-md text-gray-700 bg-white hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:text-gray-800 active:bg-gray-50 transition ease-in-out duration-150">
+        <div className="border-t border-gray-200 px-4 py-4 sm:px-6">
+        <span className="inline-flex rounded-md shadow-sm">
+          <button type="button" className="inline-flex items-center px-4 py-2 border border-gray-300 text-sm leading-5 font-medium rounded-md text-gray-700 bg-white hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:text-gray-800 active:bg-gray-50 transition ease-in-out duration-150">
             complete
           </button>
         </span>
