db216e983b09fba5a3cca4309d07c417267d29dc
stefanstifter <stefan.stifter@saswebdev.com>
2020-05-05 02:01:44 +0200

    change container styling

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import Task from './Task';
 
 function App() {
   return (
-    <div className="container mx-auto px-4 sm:px-6 lg:px-8 py-24 bg-gray-300 min-h-screen">
-      <PageHeader />
-
-      <div className="max-w-3xl mx-auto mt-6">
-        <Task title="Do the laundry" body="sort, wash and hang it up to dry." />        
+    <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 min-h-screen bg-gray-100">
+      <div className="max-w-3xl mx-auto py-12">
+        <PageHeader />        
+        <div className="mt-6">
+          <Task title="Do the laundry" body="sort, wash and hang it up to dry." />        
+        </div>
       </div>
     </div>
   );
