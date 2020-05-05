2fff9850995db912112df26b47f06ee7a94b1485
stefanstifter <stefan.stifter@saswebdev.com>
2020-05-05 21:17:02 +0200

    finetune secondary button styling. install and setup tailwindcss/ui

--- src/components/Button.js
+++ src/components/Button.js
@@ -6,7 +6,7 @@ function Button(props) {
       <button type="button" 
         onClick={ props.onClick }
         className={ 
-          "inline-flex items-center px-4 py-2 border text-sm leading-5 font-medium rounded-md focus:outline-none transition ease-in-out duration-150 " + (props.secondary ? 'border-gray-300 text-gray-500 bg-white hover:text-gray-700 hover:border-gray-500 focus:shadow-outline-blue active:text-gray-800 active:bg-gray-50' : 'border-transparent text-white bg-teal-600 hover:bg-teal-500 focus:border-teal-700 focus:shadow-outline-teal active:bg-teal-700') 
+          "inline-flex items-center px-4 py-2 border text-sm leading-5 font-medium rounded-md focus:outline-none transition ease-in-out duration-150 " + (props.secondary ? 'border-gray-300 text-gray-500 bg-white hover:text-gray-500 hover:bg-gray-50 hover:border-gray-300 active:border-gray-300 focus:shadow-outline-blue active:text-gray-800 active:bg-gray-50' : 'border-transparent text-white bg-teal-600 hover:bg-teal-500 focus:border-teal-700 focus:shadow-outline-teal active:bg-teal-700') 
         }
       >
         { props.text }
