0c77830bfef9fa2c4cbd850324c944c4d7209666
stefanstifter <stefan.stifter@saswebdev.com>
2020-05-05 02:10:50 +0200

    fake some initial tasks data

--- src/App.js
+++ src/App.js
@@ -3,12 +3,22 @@ import PageHeader from './components/PageHeader';
 import Task from './Task';
 
 function App() {
+  const data = [
+    {title: "Do the laundry", body: "Sort, wash and hang it up to dry."},
+    {title: "Go To Work", body: "Some additional..."},
+    {title: "Go To School", body: "Some additional..."},
+  ];
+
+  let taskList = data.map((task) => {
+    return <Task title={task.title} body={task.body} /> 
+  })
+
   return (
     <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 min-h-screen bg-gray-100">
       <div className="max-w-3xl mx-auto py-12">
         <PageHeader />        
         <div className="mt-6">
-          <Task title="Do the laundry" body="sort, wash and hang it up to dry." />        
+          { taskList }
         </div>
       </div>
     </div>
